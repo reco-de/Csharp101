1da93052ade50d46af008ef33dc286c5de2f3e73
Recep Yıldırım <95899401+reco-de@users.noreply.github.com>
2022-12-15 19:28:26 +0300

    Add files via upload

---
+++ donguler-while-foraech/Program.cs
@@ -0,0 +1,34 @@
+﻿// While >> istenilen koşul gerçekleşene kadar döngü devam eder.
+
+// 1 den başlayarak consle a girilen sayıların (girilen sayı da dahil) ortalamasını hesaplayan program.
+
+Console.Write("Lütfen Bir Sayı Giriniz: ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac ++;
+}
+Console.WriteLine(toplam/sayi);
+
+// 'a' dan başlayarak z ye kadar alfabeyi yazdıran program 
+
+char character = 'a';
+while (character<='z')
+{
+    Console.Write(character);
+    character ++ ;
+
+}
+
+//Foreach
+
+string [] arabalar = {"bmw","toyota","volvo"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
